30177fdd11023f6f5ea7a42a5448b9aa9ce46897
LERDOH <cthfabv09@gmail.com>
2023-02-12 16:13:11 +0200

    initial commit

--- clicker/clicker.py
+++ clicker/clicker.py
@@ -5,5 +5,14 @@ root.title("Bombclick")
 root.geometry("600x800")
 #root.iconbitmap("like.ico")
 root.resizable(False, False)
+title = tk.Label(font = ("Arial", 20, "bold"), text = "Bombclick", fg="green",relief=tk.RAISED)
+title.pack()
+coins = tk.Label(font = ("Arial", 14), text = "Coins = ", fg="orange")
+coins.pack()
+Lvl = tk.Label(font = ("Arial", 14), text = "Level = ", fg="orange")
+Lvl.pack()
+
+click_button = tk.Button(width=30, height=15) #command=click
+click_button.pack()
 
 root.mainloop()
