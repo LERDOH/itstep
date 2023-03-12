f6f941dbc90fee9b9141a28571884224e79b66c4
LERDOH <cthfabv09@gmail.com>
2023-03-12 16:41:40 +0200

    initial commit

--- Farm Clicker/Farm clicker.py
+++ Farm Clicker/Farm clicker.py
@@ -2,10 +2,9 @@ import pygame
 import random
 from threading import Timer
 
-# Инициализация Pygame
 pygame.init()
 
-
+size = (800,560)
 
 money = 50
 house1_count = 0
@@ -28,125 +27,126 @@ house5_count = 0
 house5_earnings = 0
 house5_curprice = 1000
 
-# Определение констант
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 560
 FPS = 60
 
-# Создание окна
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Простой кликер")
 bg = pygame.image.load("BG.png")
 bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.blit(bg, (0, 0))
 
-# Определение параметров кнопки
 BUTTON_WIDTH = 300
 BUTTON_HEIGHT = 100
 
-earnings = house5_earnings + house2_earnings + house3_earnings + house4_earnings + house1_earnings
+UFO_POSITION = 400
+UFO_MOVING_LEFT = False
 
-# Загрузка изображения
+earnings = house5_earnings + house2_earnings + house3_earnings + house4_earnings + house1_earnings
 image1 = pygame.image.load("farm 1.png")
 image2 = pygame.image.load("farm 2.png")
 image3 = pygame.image.load("farm 3.png")
 image4 = pygame.image.load("farm 4.png")
 image5 = pygame.image.load("farm 5.png")
+cow = pygame.image.load("corova.png")
+ufo = pygame.image.load("UFO.png")
 coin_image = pygame.image.load("Coin Image.png")
 
-# Изменение размера изображения
 image1 = pygame.transform.scale(image1, (BUTTON_WIDTH, BUTTON_HEIGHT))
 image2 = pygame.transform.scale(image2, (BUTTON_WIDTH, BUTTON_HEIGHT))
 image3 = pygame.transform.scale(image3, (BUTTON_WIDTH, BUTTON_HEIGHT))
 image4 = pygame.transform.scale(image4, (BUTTON_WIDTH, BUTTON_HEIGHT))
 image5 = pygame.transform.scale(image5, (BUTTON_WIDTH, BUTTON_HEIGHT))
+cow = pygame.transform.scale(cow, (BUTTON_WIDTH, 250))
+ufo = pygame.transform.scale(ufo, (50, 50))
+alpha = 255
 coin_image = pygame.transform.scale(coin_image, (50, 50))
 
-# Создание кнопки
 button_rect1 = pygame.Rect(10, 10, BUTTON_WIDTH, BUTTON_HEIGHT)
 button_rect2 = pygame.Rect(10, 120, BUTTON_WIDTH, BUTTON_HEIGHT)
 button_rect3 = pygame.Rect(10, 230, BUTTON_WIDTH, BUTTON_HEIGHT)
 button_rect4 = pygame.Rect(10, 340, BUTTON_WIDTH, BUTTON_HEIGHT)
 button_rect5 = pygame.Rect(10, 450, BUTTON_WIDTH, BUTTON_HEIGHT)
+button_rect6 = pygame.Rect(460, 300, 25, 25)
+button_rect7 = pygame.Rect(UFO_POSITION, 100, 25, 25)
+font = pygame.font.Font("Minecraft.ttf", 18)
+text = font.render(f"  = {money}", True, (255,255,255))
+
+house1_label = font.render(f"Amount: {house1_count}", True, (255,255,255))
+house1_cost = font.render(f"Price: {house1_curprice} ", True, (255,255,255))
+house1_earn = font.render(f"Earnings: {house1_earnings}", True, (255,255,255))
+
+house2_label = font.render(f"Amount: {house2_count}", True, (255,255,255))
+house2_cost = font.render(f"Price: {house2_curprice} ", True, (255,255,255))
+house2_earn = font.render(f"Earnings: {house2_earnings}", True, (255,255,255))
+
+house3_label = font.render(f"Amount: {house3_count}", True, (255,255,255))
+
+house3_cost = font.render(f"Price: {house3_curprice}", True, (255,255,255))
+house3_earn = font.render(f"Earnings: {house3_earnings}", True, (255,255,255))
+
+house4_label = font.render(f"Amount: {house4_count}", True, (255,255,255))
+
+house4_cost = font.render(f"Price: {house4_curprice}", True,(255,255,255))
+
+house4_earn = font.render(f"Earnings: {house4_earnings}", True, (255,255,255))
+
+
+house5_label = font.render(f"Amount: {house5_count}", True, (255,255,255))
+
+house5_cost = font.render(f"Price: {house5_curprice}", True,(255,255,255))
+
+house5_earn = font.render(f"Earnings: {house5_earnings}", True, (255,255,255))
 
 
 
-font = pygame.font.Font(None, 24)
-text = font.render(f"  = {money}", True, (30,30,30))
-
-house1_label = font.render(f"Кол-во: {house1_count}", True, (30,30,30))
-house1_cost = font.render(f"Стоимость: {house1_curprice} ", True, (30,30,30))
-house1_earn = font.render(f"Сумма добычи: {house1_earnings}", True, (30,30,30))
-
-house2_label = font.render(f"Кол-во: {house2_count}", True, (30,30,30))
-house2_cost = font.render(f"Стоимость: {house2_curprice} ", True, (30,30,30))
-house2_earn = font.render(f"Сумма добычи: {house2_earnings}", True, (30,30,30))
-
-house3_label = font.render(f"Кол-во: {house3_count}", True, (30,30,30))
-
-house3_cost = font.render(f"Стоимость: {house3_curprice}", True, (30,30,30))
-house3_earn = font.render(f"Сумма добычи: {house3_earnings}", True, (30,30,30))
-
-house4_label = font.render(f"Кол-во: {house4_count}", True, (30,30,30))
-
-house4_cost = font.render(f"Стоимость: {house4_curprice}", True, (30,30,30))
-
-house4_earn = font.render(f"Сумма добычи: {house4_earnings}", True, (30,30,30))
-
-
-house5_label = font.render(f"Кол-во: {house5_count}", True, (30,30,30))
-
-house5_cost = font.render(f"Стоимость: {house5_curprice}", True, (30,30,30))
-
-house5_earn = font.render(f"Сумма добычи: {house5_earnings}", True, (30,30,30))
-
-def update_all():
-    screen.blit(bg, (0, 0))
-    screen.blit(image1, button_rect1)
-    screen.blit(image2, button_rect2)
-    screen.blit(image3, button_rect3)
-    screen.blit(image4, button_rect4)
-    screen.blit(image5, button_rect5)
-    screen.blit(house1_cost, (button_rect5.x + 110, button_rect1.y + 35))
-    screen.blit(house1_label, (button_rect5.x + 110, button_rect1.y+15))
-    screen.blit(house1_earn, (button_rect5.x + 110, button_rect1.y + 55))
-    screen.blit(house2_label, (button_rect2.x + 110, button_rect2.y+15))
-    screen.blit(house2_cost, (button_rect2.x + 110, button_rect2.y + 35))
-    screen.blit(house2_earn, (button_rect2.x + 110, button_rect2.y + 55))
-    screen.blit(house3_label, (button_rect3.x + 110, button_rect3.y+15))
-    screen.blit(house3_cost, (button_rect3.x + 110, button_rect3.y + 35))
-    screen.blit(house3_earn, (button_rect3.x + 110, button_rect3.y + 55))
-    screen.blit(house4_label, (button_rect4.x + 110, button_rect4.y+15))
-    screen.blit(house4_cost, (button_rect4.x + 110, button_rect4.y + 35))
-    screen.blit(house4_earn, (button_rect4.x + 110, button_rect4.y + 55))
-    screen.blit(house5_label, (button_rect5.x + 110, button_rect5.y+15))
-    screen.blit(house5_cost, (button_rect5.x + 110, button_rect5.y + 35))
-    screen.blit(house5_earn, (button_rect5.x + 110, button_rect5.y + 55))
-    screen.blit(coin_image, (SCREEN_WIDTH-150, 10))
-    screen.blit(text, (SCREEN_WIDTH-150 + 50, 25))
-
-
-update_all()
-
+#update_all()
 def autoclick ():
     global money
     global text
     money += house5_earnings + house2_earnings + house3_earnings + house4_earnings + house1_earnings
-    text = font.render(f" = {money}", True, (30, 30, 30))
-    update_all()
+    text = font.render(f" = {money}", True, (200, 200, 200))
+    #update_all()
     Timer(0.5, autoclick).start()
 
 autoclick()
+def UFO_Moving():
+        global UFO_POSITION, UFO_MOVING_LEFT, button_rect7
+        if UFO_POSITION <= 400:
+            UFO_MOVING_LEFT = False
+            print("134")
+        elif UFO_POSITION >= 560:
+            print("431")
+            UFO_MOVING_LEFT = True
+        if UFO_MOVING_LEFT:
+            UFO_POSITION -= 1
+        else:
+            UFO_POSITION += 1
+        button_rect7 = pygame.Rect(UFO_POSITION, 100, BUTTON_WIDTH, 250)
+        Timer(0.1, UFO_Moving).start()
 
-# Бесконечный цикл игры
+
+UFO_Moving()
+
+def UFO_TRAN2():
+    global alpha
+    alpha = 255
+    ufo.set_alpha(alpha)
+def UFO_TRAN():
+    global alpha
+    alpha = 0
+    ufo.set_alpha(alpha)
+    Timer(10, UFO_TRAN2).start()
+
+
+
 running = True
 while running:
-    # Обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            # Обработка события нажатия кнопки
             if button_rect1.collidepoint(event.pos):
                 print("Кнопка 1 нажата!")
                 if money >= house1_curprice:
@@ -154,12 +154,11 @@ while running:
                     house1_count += 1
                     house1_earnings += 1
                     house1_curprice += 10
-                    screen.fill((30, 30, 30))
-                    house1_label = font.render(f"Кол-во: {house1_count}", True, (30,30,30))
-                    house1_cost = font.render(f"Стоимость: {house1_curprice} ", True, (30, 30, 30))
-                    house1_earn = font.render(f"Сумма добычи: {house1_earnings}", True, (30, 30, 30))
-                    text = font.render(f" = {money}", True, (30,30,30))
-                    update_all()
+                    house1_label = font.render(f"Кол-во: {house1_count}", True, (255,255,255))
+                    house1_cost = font.render(f"Стоимость: {house1_curprice} ", True, (255, 255, 255))
+                    house1_earn = font.render(f"Сумма добычи: {house1_earnings}", True, (255, 255, 255))
+                    text = font.render(f" = {money}", True, (255,255,255))
+                    #update_all()
             elif button_rect2.collidepoint(event.pos):
                 print("Кнопка 2 нажата!")
                 if money >= house2_curprice:
@@ -167,12 +166,11 @@ while running:
                     house2_count += 1
                     house2_earnings += 5
                     house2_curprice += 50
-                    screen.fill((30, 30, 30))
-                    house2_label = font.render(f"Кол-во: {house2_count}", True, (30, 30, 30))
-                    house2_cost = font.render(f"Стоимость: {house2_curprice} ", True, (30, 30, 30))
-                    house2_earn = font.render(f"Сумма добычи: {house2_earnings}", True, (30, 30, 30))
-                    text = font.render(f" = {money}", True, (30,30,30))
-                    update_all()
+                    house2_label = font.render(f"Кол-во: {house2_count}", True, (255, 255, 255))
+                    house2_cost = font.render(f"Стоимость: {house2_curprice} ", True, (255, 255, 255))
+                    house2_earn = font.render(f"Сумма добычи: {house2_earnings}", True, (255, 255, 255))
+                    text = font.render(f" = {money}", True, (255,255,255))
+                    #update_all()
             elif button_rect3.collidepoint(event.pos):
                 print("Кнопка 3 нажата!")
                 if money >= house3_curprice:
@@ -180,12 +178,11 @@ while running:
                     house3_count += 1
                     house3_earnings += 15
                     house3_curprice += 250
-                    screen.fill((30, 30, 30))
-                    house3_label = font.render(f"Кол-во: {house3_count}", True, (30, 30, 30))
-                    house3_cost = font.render(f"Стоимость: {house3_curprice} ", True, (30, 30, 30))
-                    house3_earn = font.render(f"Сумма добычи: {house3_earnings}", True, (30, 30, 30))
-                    text = font.render(f" = {money}", True, (30,30,30))
-                    update_all()
+                    house3_label = font.render(f"Кол-во: {house3_count}", True, (255, 255, 255))
+                    house3_cost = font.render(f"Стоимость: {house3_curprice} ", True, (255, 255, 255))
+                    house3_earn = font.render(f"Сумма добычи: {house3_earnings}", True, (255, 255, 255))
+                    text = font.render(f" = {money}", True, (255,255,255))
+                    #update_all()
             elif button_rect4.collidepoint(event.pos):
                 print("Кнопка 4 нажата!")
                 if money >= house4_curprice:
@@ -193,12 +190,11 @@ while running:
                     house4_count += 1
                     house4_earnings += 50
                     house4_curprice += 500
-                    screen.fill((30, 30, 30))
-                    house4_label = font.render(f"Кол-во: {house4_count}", True, (30, 30, 30))
-                    house4_cost = font.render(f"Стоимость: {house4_curprice} ", True, (30, 30, 30))
-                    house4_earn = font.render(f"Сумма добычи: {house4_earnings}", True, (30, 30, 30))
-                    text = font.render(f" = {money}", True, (30,30,30))
-                    update_all()
+                    house4_label = font.render(f"Кол-во: {house4_count}", True, (255, 255, 255))
+                    house4_cost = font.render(f"Стоимость: {house4_curprice} ", True, (255, 255, 255))
+                    house4_earn = font.render(f"Сумма добычи: {house4_earnings}", True, (255, 255, 255))
+                    text = font.render(f" = {money}", True, (255,255,255))
+                    #update_all()
             elif button_rect5.collidepoint(event.pos):
                 print("Кнопка 5 нажата!")
                 if money >= house5_curprice:
@@ -206,18 +202,49 @@ while running:
                     house5_count += 1
                     house5_earnings += 100
                     house5_curprice += 1000
-                    screen.fill((30, 30, 30))
-                    house5_label = font.render(f"Кол-во: {house5_count}", True, (30, 30, 30))
-                    house5_cost = font.render(f"Стоимость: {house5_curprice} ", True, (30, 30, 30))
-                    house5_earn = font.render(f"Сумма добычи: {house5_earnings}", True, (30, 30, 30))
-                    text = font.render(f" = {money}", True, (30,30,30))
-                    update_all()
+                    house5_label = font.render(f"Кол-во: {house5_count}", True, (255, 255, 255))
+                    house5_cost = font.render(f"Стоимость: {house5_curprice} ", True, (255, 255, 255))
+                    house5_earn = font.render(f"Сумма добычи: {house5_earnings}", True, (255, 255, 255))
+                    text = font.render(f" = {money}", True, (255,255,255))
+                    #update_all()
+            elif button_rect6.collidepoint(event.pos):
+                print("Корова нажата!")
+                money += 3
+                #update_all()
+            elif button_rect7.collidepoint(event.pos) and alpha == 255:
+                print("Нло нажат!")
+                UFO_TRAN()
+                money += 10
+                #update_all()
 
-    # Отрисовка экрана
-    pygame.display.update()
+    screen.blit(bg, (0, 0))
+    screen.blit(image1, button_rect1)
+    screen.blit(image2, button_rect2)
+    screen.blit(image3, button_rect3)
+    screen.blit(image4, button_rect4)
+    screen.blit(image5, button_rect5)
+    screen.blit(cow, button_rect6)
+    screen.blit(ufo, button_rect7)
+    screen.blit(house1_cost, (button_rect5.x + 110, button_rect1.y + 35))
+    screen.blit(house1_label, (button_rect5.x + 110, button_rect1.y + 15))
+    screen.blit(house1_earn, (button_rect5.x + 110, button_rect1.y + 55))
+    screen.blit(house2_label, (button_rect2.x + 110, button_rect2.y + 15))
+    screen.blit(house2_cost, (button_rect2.x + 110, button_rect2.y + 35))
+    screen.blit(house2_earn, (button_rect2.x + 110, button_rect2.y + 55))
+    screen.blit(house3_label, (button_rect3.x + 110, button_rect3.y + 15))
+    screen.blit(house3_cost, (button_rect3.x + 110, button_rect3.y + 35))
+    screen.blit(house3_earn, (button_rect3.x + 110, button_rect3.y + 55))
+    screen.blit(house4_label, (button_rect4.x + 110, button_rect4.y + 15))
+    screen.blit(house4_cost, (button_rect4.x + 110, button_rect4.y + 35))
+    screen.blit(house4_earn, (button_rect4.x + 110, button_rect4.y + 55))
+    screen.blit(house5_label, (button_rect5.x + 110, button_rect5.y + 15))
+    screen.blit(house5_cost, (button_rect5.x + 110, button_rect5.y + 35))
+    screen.blit(house5_earn, (button_rect5.x + 110, button_rect5.y + 55))
+    screen.blit(coin_image, (SCREEN_WIDTH - 150, 10))
+    screen.blit(text, (SCREEN_WIDTH - 150 + 50, 25))
 
-    # Ограничение частоты обновления экрана
+    pygame.display.flip()
+
     pygame.time.Clock().tick(FPS)
 
-# Завершение Pygame
 pygame.quit()
--- main.py
+++ main.py
@@ -1,15 +1,62 @@
-import tkinter as tk
+import pygame
 
-root = tk.Tk()
-root.title("Bombclick")
-root.geometry("600x825")
-root.resizable(False, False)
+# Инициализация Pygame
+pygame.init()
 
-M_images = [tk.PhotoImage(file="lvl1.png"), tk.PhotoImage(file="lvl2.png")]
+# Размеры окна
+screen_width = 400
+screen_height = 400
 
-#lvl_1 = tk.PhotoImage(file="lvl1.png")
+# Создание окна
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Передвигаемый квадрат")
 
-print (M_images[0])
-print (M_images[1])
+# Цвета
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-root.mainloop()
+# Размеры и положение квадрата
+square_size = 50
+square_x = (screen_width - square_size) // 2
+square_y = (screen_height - square_size) // 2
+
+# Главный цикл программы
+running = True
+while running:
+    # Обработка событий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Получение координат мыши и перемещение квадрата
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    if pygame.mouse.get_pressed()[0]:
+
+        # Проверяем, находится ли курсор внутри квадрата
+        if square_x <= mouse_x <= square_x + square_size and \
+                square_y <= mouse_y <= square_y + square_size:
+            square_x = mouse_x - square_size // 2
+            square_y = mouse_y - square_size // 2
+            # Проверка, не касается ли квадрат края экрана
+    if square_x < 0 or square_x + square_size > screen_width or \
+                    square_y < 0 or square_y + square_size > screen_height:
+                square_x = -square_size
+                square_y = -square_size
+                time_since_disappearance = pygame.time.get_ticks()
+
+            # Проверка, прошло ли уже 10 секунд с момента исчезновения квадрата
+    if square_x == -square_size and square_y == -square_size:
+        time_elapsed = pygame.time.get_ticks() - time_since_disappearance
+        if time_elapsed >= 100:
+                    square_x = screen_width // 2 - square_size // 2
+                    square_y = screen_height // 2 - square_size // 2
+
+    # Отрисовка фона и квадрата
+    screen.fill(white)
+    pygame.draw.rect(screen, black, (square_x, square_y, square_size, square_size))
+
+    # Обновление экрана
+    pygame.display.update()
+
+# Завершение работы Pygame
+pygame.quit()
